dbe72e9d19190cd40ec15a3db3a869ca9bbca323
Susana <susa.hl37@gmail.com>
2020-12-02 18:17:57 -0300

    crud para modulo turnos

--- paciente/forms.py
+++ paciente/forms.py
@@ -1,34 +1,18 @@
 from django import forms
-from paciente.models import Paciente
+from paciente.models import Paciente,Turnos
 from users.models import User
 
 
-class PacienteForm(forms.Form):
-    GENERO_TYPE_CHOICES = (
-        ('F',"FEMENINO"),
-        ('M',"MASCULINO")
-    )
-    nombre = forms.CharField(max_length=100)
-    apellido = forms.CharField()
-    edad = forms.IntegerField()
-    fechaNacimiento = forms.DateField()
-    email = forms.EmailField()
-    genero = forms.ChoiceField(choices=GENERO_TYPE_CHOICES)
-    telefono =forms.CharField()
-    direccion=forms.CharField(max_length=300)
-    documentoIdentidad=forms.CharField()
-    estatura=forms.DecimalField(max_digits=6,decimal_places=2)
-    peso=forms.DecimalField(max_digits=6,decimal_places=2)
-
-class TurnosForm(forms.Form):
-    ASISTENCIA_CHOICES = (
-        (1,"Si"),
-        (2,"No")
-    )
-    id_paciente=forms.ModelChoiceField(queryset=Paciente.objects.all(),empty_label="Selecciona un paciente",label="Paciente") 
-    id_usuario=forms.ModelChoiceField(queryset=User.objects.filter(rol=2),empty_label="Selecciona médico",label="Médico") 
-    fecha=forms.DateField()
-    hora=forms.TimeField()
-    asistencia=forms.TypedChoiceField(choices=ASISTENCIA_CHOICES,coerce = str)
-
-
+class PacienteForm(forms.ModelForm):
+    class Meta:
+          model = Paciente
+          fields =  "__all__"
+  
+class TurnosForm(forms.ModelForm):
+    class Meta:
+          model = Turnos
+          widgets = {
+            'fecha': forms.DateInput(attrs={'class':'datepicker'}),
+          }
+          fields = '__all__'
+          
